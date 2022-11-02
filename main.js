e4eb6ee6d3b623e688f4f038a4bfe833827b57f8
crbannister <crbannister@gmail.com>
2022-11-02 16:36:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function parseSRC() {
+    const srcURLObj = new URL(document.getElementById("title").getAttribute("src"))
+    const urlParams = new URLSearchParams(srcURLObj.search)
+    document.getElementById("entityType").innerHTML = "Entity Type: " + urlParams.get("EntityType")
+    document.getElementById("entityID").innerHTML = "Entity ID: " + urlParams.get("EntityID")
+    document.getElementById("userID").innerHTML = "Bullhorn User ID: " + urlParams.get("UserID")
+    document.getElementById("corporationID").innerHTML = "Corporation ID: " + urlParams.get("CorporationID")
+    document.getElementById("privateLabelID").innerHTML = "Private Label ID: " + urlParams.get("PrivateLabelID")
+
+    //alert(JSON.stringify(urlParams.get("UserID")))
+}
