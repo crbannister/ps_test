9376c8d9fc69c02de371a4e3789abb99be4cf7e4
crbannister <crbannister@gmail.com>
2022-11-03 15:13:40 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 function parseSRC() {
-    const iframe = window.location
-    console.log("frame: ", iframe)
-    const srcURLObj = new URL(iframe)
+    const window = window
+    console.log("window: ", window)
+    const srcURLObj = new URL(iframe.src)
     const urlParams = new URLSearchParams(srcURLObj.search)
     document.getElementById("entityType").innerHTML = "Entity Type: " + urlParams.get("EntityType")
     document.getElementById("entityID").innerHTML = "Entity ID: " + urlParams.get("EntityID")
     document.getElementById("userID").innerHTML = "Bullhorn User ID: " + urlParams.get("UserID")
     document.getElementById("corporationID").innerHTML = "Corporation ID: " + urlParams.get("CorporationID")
     document.getElementById("privateLabelID").innerHTML = "Private Label ID: " + urlParams.get("PrivateLabelID")
-
-    //alert(JSON.stringify(urlParams.get("UserID")))//
+    document.getElementById("payAdjustmentsBtn").href = "https://hrpdev.kellyservices.com/?entityID=" + urlParams.get("EntityID") + "&userID=" + urlParams.get("UserID")
 }
