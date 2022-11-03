eac672a3a8ec2ac385f841e9786650b4828b42a3
crbannister <crbannister@gmail.com>
2022-11-03 15:16:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function parseSRC() {
-    const window = window
-    console.log("window: ", window)
+    const iframe = window.location
+    console.log("window: ", iframe)
     const srcURLObj = new URL(iframe.src)
     const urlParams = new URLSearchParams(srcURLObj.search)
     document.getElementById("entityType").innerHTML = "Entity Type: " + urlParams.get("EntityType")
